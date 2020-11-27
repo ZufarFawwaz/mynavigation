ecd0a94e15ed8d8ecd37e8893a6bd82dce387150
ZufarFawwaz <zufarkuntong@gmail.com>
2020-11-27 07:48:11 +0700

    first commit

---
+++ app/src/main/java/com/zufar/mynavigation/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.zufar.mynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynavigation.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
